d3a33447c4c7a63c02247d96b6144aec76a56b2a
olasoytena <olasoytena@gmail.com>
2024-04-06 14:51:34 -0600

    Finished About me and Portfolio section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Intro from './components/intro';
+import Contact from './components/contact';
+import Footer from "./components/footer";
+import Portfolio from './components/portfolio';
+import Timeline from './components/timeline';
+
+function App() {
+
+  return (
+    <div className='max-w-5xl w-11/12 mx-auto'>
+      <Intro/>
+      <Portfolio/>
+      <Timeline/>
+      <Contact/>
+      <Footer/>
+       </div>
+  )
+}
+
+export default App
---
+++ src/data/portfolio.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Portfolio Website',
+        imgUrl: '/assets/temporaryPage.png',
+        stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+        link: 'http://localhost:5173',
+    },
+    {
+        title: 'Graphics Terrain Generator',
+        imgUrl: '/assets/Terrain.png',
+        stack: ['OpenGL', 'C'],
+        link: 'https://github.com/olasoytena/GraphicsTerrainGenerator',
+    },
+    {
+        title: 'Traffic Light Recognition',
+        imgUrl: '/assets/model_accuracy.png',
+        stack: ['Python', 'Tensorflow', 'Jupiter Notebook', 'Machine Learning'],
+        link: 'https://github.com/olasoytena/TrafficLightRecognition',
+    },
+    {
+        title: 'Portfolio Website',
+        imgUrl: '/assets/temporaryPage.png',
+        stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+        link: 'http://localhost:5173',
+    },
+    
+]
