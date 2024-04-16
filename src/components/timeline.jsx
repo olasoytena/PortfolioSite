1dd38dbbaf13b3931a2daf3a53b330cd5593c7d3
olasoytena <olasoytena@gmail.com>
2024-04-16 16:18:28 -0600

    Made better for mobile

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -9,7 +9,7 @@ function Timeline() {
         <div className="flex flex-col md:flex-row  justify-left w-full ">
             <div className="w-1/12"> </div>
 
-            <div className="grid grid-cols-1 w-1/3 max-h-min bg-white mt-6 pb-4 pl-4 pr-4 pt-4 rounded-lg border-4 
+            <div className="grid grid-cols-1 md:w-1/3 max-h-min bg-white mt-6 pb-4 pl-4 pr-4 pt-4 rounded-lg border-4 
             border-yellow-950 gap-3">
             <h2 className="text-xl md:text-2xl font-bold ">Education</h2>
                 {education.map(el => (
@@ -22,7 +22,7 @@ function Timeline() {
             </div>
             
             <div className="w-1/12"> </div>
-            <div className="grid grid-cols-1 gap-4 w-1/2 bg-white mt-6 pb-4 pl-4 pr-4 pt-4 rounded-lg border-4 
+            <div className="grid grid-cols-1 gap-4 md:w-1/2 bg-white mt-6 pb-4 pl-4 pr-4 pt-4 rounded-lg border-4 
             border-yellow-950 gap-2">
                 <h2 className="text-xl md:text-2xl font-bold pt-6">Work History</h2>
                 {workHistory.map(item => (
